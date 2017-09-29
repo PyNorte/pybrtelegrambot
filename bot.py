a51d4e3aceed235f304ee8611522dc3540753fda
Nilo Menezes <nilo.menezes@gmail.com>
2017-09-29 12:05:44 +0200

    Atualizacao da API, suporte a mensagens grandes, fix da protecao de flood

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ import telebot
 import os
 import logging
 import pytz
-from datetime import datetime, timedelta
 
+from datetime import datetime, timedelta
 
 import db
 from mensagens import *
@@ -27,53 +27,86 @@ palmas = pytz.timezone("America/Araguaina")
 
 # Hora em que o último aviso foi postado no grupo
 # Usado para evitar que usuários abusem do espaço do grupo para comandos
-ultimo_aviso = None
-ultimo_novo = None
+ultima_mensagem = {}
+# Tempo para desativar a protecao de spam/flood em minutos
+TEMPO_ENTRE_MENSAGENS = 15
+# Tamanho maximo do texto a enviaer em uma unica mensagem
+TAMANHO_MAXIMO = 4000
 
 
 def em_grupo(mensagem):
+    """Retorna True se a mensagem foi passada no grupo"""
     return mensagem.chat.type in ["group", "supergroup"]
 
 def destino(mensagem):
-    #if mensagem.from_user:
-    #    return mensagem.from_user.id
+    """Extrai o id de destino a partir de mensagem"""
     return mensagem.chat.id
 
+def quebra_mensagem(mensagem):
+    """Quebra mensagens grandes para serem enviadas pela API.
+       Como o Telegram define um maximo de caracteres,
+       quebra uma mensagem em varias pequenas."""
+    linhas = mensagem.splitlines()
+    texto = []
+    tamanho = 0
+    for linha in linhas:
+        if tamanho + len(linha) < TAMANHO_MAXIMO:
+            texto.append(linha)
+            tamanho += len(linha) + 1
+        else:
+            if texto:
+                yield "\n".join(texto)
+            while len(linha) > TAMANHO_MAXIMO:
+                yield linha[:TAMANHO_MAXIMO]
+                linha = linha[TAMANHO_MAXIMO:]
+            texto = [linha]
+            tamanho = len(linha) + 1
+    if texto:
+        yield "\n".join(texto)
+
+
 def bot_responda(mensagem, resposta):
+    """Telegram nao aceita mensagens com mais de 5000 caracteres"""
     chat_id = destino(mensagem)
-    bot.send_message(chat_id, resposta)
+    for r in quebra_mensagem(resposta):
+        bot.send_message(chat_id, r)
+
 
 def nome(mensagem):
+    """Retorna o nome do usuario (first_name) ou o username caso este nao tenha sido entrado"""
     nome = mensagem.from_user.first_name
     if not nome:
         return mensagem.from_user.username
     return nome
 
 
-def protecao_spam_do_grupo(mensagem):
-    global ultimo_aviso
+def protecao_spam_do_grupo(mensagem, tipo):
+    """Verifica se o mesmo comando foi repedito no grupo. Usado para evitar que o chat do
+       grupo seja dominado pelas respostas do bot."""
+    global ultima_mensagem
     if em_grupo(mensagem):
-        if not ultimo_aviso or datetime.now() - ultimo_aviso > timedelta(minutes=15):
+        ultimo = ultima_mensagem.get(tipo)
+        if ultimo and datetime.now() - ultimo > timedelta(minutes=TEMPO_ENTRE_MENSAGENS):
             bot_responda(mensagem, BOT_PRIVADO)
-            ultimo_aviso = datetime.now()
-        return True
-    else:
-        return False
+            return True
+        ultima_mensagem[tipo] = datetime.now()
+    return False
 
 @bot.message_handler(content_types=['new_chat_participant'])
 def send_novo(message):
-    global ultimo_novo
+    """Mensagem enviada para novos integrantes do grupo"""
     nome_u = nome(message)
-    if not ultimo_novo or datetime.now() - ultimo_novo > timedelta(minutes=5):
+    if not protecao_spam_do_grupo(message, "novo"):
         bot_responda(message, START.format(nome_u))
     else:
         bot_responda(message, START_REPETIDO.format(nome_u))
-    ultimo_novo = datetime.now()
+
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    if protecao_spam_do_grupo(message):
+    """Mensagem de boas vindas"""
+    if protecao_spam_do_grupo(message, "start"):
         return
     bot_responda(message, START.format(nome(message)))
 
@@ -81,47 +114,55 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['help', 'ajuda'])
 def send_help(message):
-    if protecao_spam_do_grupo(message):
+    """Mensagem com ajuda sobre os comandos do bot"""
+    if protecao_spam_do_grupo(message, "help"):
         return
     bot_responda(message, AJUDA)
 
 
 @bot.message_handler(commands=['link', 'links'])
 def send_link(message):
-    if protecao_spam_do_grupo(message):
+    if protecao_spam_do_grupo(message, "links"):
         return
     bot_responda(message, LINKS)
 
 
 @bot.message_handler(commands=['estados'])
 def send_estados(message):
-    if protecao_spam_do_grupo(message):
+    """Lista de estados"""
+    if protecao_spam_do_grupo(message, "estados"):
         return
     bot_responda(message, LISTA_DE_ESTADOS)
 
 
 @bot.message_handler(commands=['whoami'])
 def send_whoami(message):
-    if protecao_spam_do_grupo(message):
+    """Informacors sobre o usuario atual"""
+    if protecao_spam_do_grupo(message, "whoami"):
         return
     bot_responda(message, WHOAMI.format(message.from_user))
 
 
 @bot.message_handler(commands=['lista'])
 def send_lista(message):
+    """Envia a lista de usuarios cadastrados"""
+    if protecao_spam_do_grupo(message, "lista"):
+        return
     bot_responda(message, db.lista_users())
 
 
 @bot.message_handler(commands=['nomes', 'membros'])
 def send_nomes(message):
-    if protecao_spam_do_grupo(message):
+    """Envia os nomes do cadastro"""
+    if protecao_spam_do_grupo(message, "membros"):
         return
     bot_responda(message, db.lista_users_por_nome())
 
 
 @bot.message_handler(commands=['estatistica', 'contador', 'total', 'stat', 'stats'])
 def send_stats(message):
-    if protecao_spam_do_grupo(message):
+    """Estatisticas do cadastro por estadp"""
+    if protecao_spam_do_grupo(message, "stats"):
         return
     stats = db.get_stats()
     mensagem = STAT_CAB
@@ -133,7 +174,8 @@ def send_stats(message):
 
 @bot.message_handler(commands=['eventos'])
 def send_eventos(message):
-    if protecao_spam_do_grupo(message):
+    """Envia a lista dos eventos atuais"""
+    if protecao_spam_do_grupo(message, "eventos"):
         return
     eventos = db.get_eventos()
     mensagem = EVENTOS_CAB
@@ -149,8 +191,7 @@ def send_eventos(message):
 
 @bot.message_handler(commands=['membro', 'mecadastra', 'novo'])
 def send_membro(message):
-    #if protecao_spam_do_grupo(message):
-    #    return
+    """Permite ao usuario se cadastrar e informar seu estado"""
     params = message.text.split()
     if len(params) < 2:
         bot_responda(message, MEMBRO_AJUDA)
@@ -172,7 +213,8 @@ def send_membro(message):
 
 @bot.message_handler(commands=['hora', 'horas', 'agora', 'now'])
 def send_hora(message):
-    if protecao_spam_do_grupo(message):
+    """Envia a hora local em varias cidades do norte"""
+    if protecao_spam_do_grupo(message, "hora"):
         return
     agora = datetime.utcnow().replace(tzinfo=pytz.utc)
     horarios = {
@@ -185,9 +227,11 @@ def send_hora(message):
     bot_responda(message, HORA.format(**horarios))
 
 
+# Principal
 _logger = telebot.logger
 telebot.logger.setLevel(logging.DEBUG)
 
+# Inicia o pooling da api
 bot.polling(none_stop=True)
 
 
